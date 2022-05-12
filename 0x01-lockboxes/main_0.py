c200db0b50eb6eeb34b067b30b617b1da8536c90
Laurajcb <callejaslaura96@gmail.com>
2022-05-12 13:24:59 -0500

    Update repo with gitignore and added task utf-8

---
+++ 0x01-lockboxes/main_0.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
+
+boxes = None
+print(canUnlockAll(boxes))
+
+
+boxes = []
+keys = []
+for n in range(1, 1000):
+    keys = []
+    for m in range(1, 1000):
+        keys.append(m)
+    boxes.append(keys)
+
+print(canUnlockAll(boxes))
