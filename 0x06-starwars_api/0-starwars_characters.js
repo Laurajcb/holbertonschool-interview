157920a68511475a0053b8b83c351471f1382e86
Laurajcb <callejaslaura96@gmail.com>
2022-05-26 13:26:29 -0500

    fix semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const filmId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
